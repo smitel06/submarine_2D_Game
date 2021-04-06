2cae767b822de100f45d917dca9d981eba748dd8
Eli <smitel06@hotmail.com>
2021-04-06 16:39:01 +0930

    Added player control + cam follow

---
+++ 2D_Submarine_Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject mCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //camera follows player keeping player to centre of screen
+        mCam.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -5);
+
+    }
+}
--- 2D_Submarine_Game/Assets/Scripts/PlayerController.cs
+++ 2D_Submarine_Game/Assets/Scripts/PlayerController.cs
@@ -10,9 +10,28 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    //public variables to control speed
+    public float movementSpeed;
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+       //controls for movement 
+       if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))//move up 
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.up * Time.deltaTime * movementSpeed);
+        }
+       else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))///move down
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.up * Time.deltaTime * movementSpeed * -1); //opposite force for down movement so we use - 1
+        }
+       else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))//move right
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.right * Time.deltaTime * movementSpeed);
+        }
+       else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))//move left
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.right * Time.deltaTime * movementSpeed * - 1);
+        }
         
     }
 }
