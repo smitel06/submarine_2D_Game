d3ed1f995a40d48a8be96b9e1d231940939df09b
Eli <smitel06@hotmail.com>
2021-04-25 15:49:18 +0930

    Fixed laser + added shark

---
+++ 2D_Submarine_Game/Assets/Scripts/shark.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class shark : MonoBehaviour
+{
+    
+    public AIPath aiPath;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //flip the shark if its going left
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(0.5f, 0.5f, 1);
+        }
+        else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-0.5f, 0.5f, 1);
+        }
+
+        //rotate the sharkf
+
+    }
+}
